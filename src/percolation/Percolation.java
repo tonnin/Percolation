71f330eaaf811cb505846bcce74c0364d8e026a9
Tonnin <axrfo@MORLF564.agfahealthcare.com>
2014-09-22 19:18:01 +0200

    nn

--- src/percolation/Percolation.java
+++ src/percolation/Percolation.java
@@ -13,6 +13,7 @@ public class Percolation {
 	private static int CLOSE = 0;
 	private static int OPEN = 1;
 	private static int FULL = 2;
+	private int opennodes;
 	int[][] gride;
 
 	/**
@@ -20,38 +21,54 @@ public class Percolation {
 	 */
 	public Percolation(int N) {
 		if (N <= 0) {
-			throw new IndexOutOfBoundsException("Illegal parameter value.");
+			throw new IllegalArgumentException("Illegal parameter value.");
 		} else {
 			for (int i = 0; i < N; i++) {
 				gride[i][i] = CLOSE;
 			}
+			opennodes = 0;
 		}
 	}
 
 	public void open(int i, int j) {
 		// TODO
-		if (isOpen(i, j)) {
-			// Nothing
-			null;
-		} else {
-			gride[i][j] = OPEN;
+		if (isIndexValid(i) && isIndexValid(j)) {
+			if (!isOpen(i, j)) {
+				gride[i][j] = OPEN;
+				opennodes++;
+			}
+			// Should i add the else
 		}
 	}
 
 	public boolean isOpen(int i, int j) {
-		if (gride[i][j] == OPEN) {
-			return true;
-		} else {
-			return false;
+		boolean flag = false;
+		if (isIndexValid(i) && isIndexValid(j)) {
+			if (gride[i][j] == OPEN) {
+				flag = true;
+			} else {
+				flag = false;
+			}
 		}
+		return flag;
 	}
 
 	public boolean isFull(int i, int j) {
 		// TODO
+		boolean flag = false;
+		if (isIndexValid(i) && isIndexValid(j)) {
+			if (gride[i][j] == FULL) {
+				flag = true;
+			} else {
+				flag = false;
+			}
+		}
+		return flag;
 	}
 
 	public boolean percolate() {
 		// TODO
+		return true;
 	}
 
 	/**
